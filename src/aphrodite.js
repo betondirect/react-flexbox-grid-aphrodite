6a59e9b9f5e79a5c2b2ba6f6f4bf3fe9e1e67369
Theophile Batoz <theophile.batoz@gmail.com>
2017-09-05 22:19:27 +0200

    build

--- src/aphrodite.js
+++ src/aphrodite.js
@@ -1,2 +1,2 @@
-export {css} from 'aphrodite-local-styles/no-important'
-export {StyleSheet} from 'aphrodite-local-styles'
+export { css } from 'aphrodite-local-styles/no-important';
+export { StyleSheet } from 'aphrodite-local-styles';
